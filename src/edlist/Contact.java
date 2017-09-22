40e77c271019df989b34d7b19cfff9ae88bd5c9a
Jorge Ricardo Cabrales Reyes <A01410342@itesm.mx>
2017-09-22 00:01:51 -0500

    actividad

---
+++ src/edlist/Contact.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edlist;
+
+/**
+ *
+ * @author JorgeRicardo
+ */
+public class Contact {
+    String nombre;
+    String telefono;
+    
+    public Contact(){
+        this.nombre="hola";
+        this.telefono="mundo";
+    }
+    
+    @Override
+}
---
+++ src/edlist/EDList.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edlist;
+
+/**
+ *
+ * @author JorgeRicardo
+ */
+public class EDList {
+
+    /**
+     * @param args the command line arguments
+     */
+   /* public static void main(String[] args) {
+    MyList <String> Lista = new  MyList();
+    Lista.insertFirst("127");
+    Lista.insertLast("200");
+    Lista.insertFirst("20");
+    Lista.insertLast("300");
+    Lista.showList();
+    Lista.deleteNode("300");
+    Lista.showList();
+    }*/
+    
+}
